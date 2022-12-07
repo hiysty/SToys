74018b7429bd573a1f14eb17ed84924f4895440a
FecrSqraper12 <yahya.ara@outloook.com>
2022-12-07 09:45:15 +0300

    test commit

--- swap_toys/lib/page/profile_page.dart
+++ swap_toys/lib/page/profile_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+//profil
+
 class ProfilePage extends StatelessWidget {
   static final user = FirebaseAuth.instance.currentUser!;
 
